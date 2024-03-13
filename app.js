f52e01c0c28f74c787413e2657b90ed2842ed1bd
@jrdp2706! <celc05lerdo@gmail.com>
2024-03-13 08:44:06 -0600

    se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,27 @@
+var num1 = 7346786;
+var num2 = 456465;
+
+var nombre = "julio"
+console.log("mi nombre es:" , nombre , "y he realizado las siguientes operaciones matematicas")
+
+var suma = num1 + num2;
+console.log("el resulrtado de la suma es:" , suma);
+
+var resta = num1 - num2;
+console.log( "el resultado de la resta es:" , resta);
+
+var multiplicacion = num1 * num2;
+console.log("el resultado de la multiplicacion es:" , multiplicacion)
+
+var division = num1 / num2;
+console.log("el resultado de la division es:" , division)
+
+
+// 7 8 6 sacar promedio de las calificaciones 
+
+var num3 = 7
+var num4 = 8
+var num5 = 3 
+
+var promedio = (num3 + num4 + num5)/3;
+console.log("la calificaciion de Elias es" ,promedio ) 
